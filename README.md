bfed9f3c7ae1cc0848c7caf9477b332bcb4be5aa
ccli8 <devnull@localhost>
2016-11-01 01:15:21 +0000

    Empty example; ; To be extended later

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting started with NuMaker Brick on mbed OS
+
+This is an example for access to NuMaker Brick I2C slave devices.
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "mbed.h"
+
+int main() {
+    
+    printf("Example for access to NuMaker Brick I2C slave devices\n");
+    return 0;
+}
+
